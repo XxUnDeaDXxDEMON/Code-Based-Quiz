22a13e432af084d4c2e84a4f41783e96e2ec7080
Richard <rasteavens@yahoo.com>
2021-06-03 20:56:44 -0700

    Java set up but not working yet

---
+++ assets/questions.js
@@ -0,0 +1,93 @@
+var questions = [
+    {
+      title: "What tag is used to define a table or image notation (caption)?",
+      choices: ["<!DOCTYPE>", "<caption>", "<embed>", "<code>"],
+      answer: "<caption>"
+    },
+    {
+      title: "What tag is used to define a hyperlink, or link to another page?",
+      choices: [
+        "<a>",
+        "<blockquote>",
+        "<strong>",
+        "<em>"
+      ],
+      answer: "<a>"
+    },
+    {
+      title: "What declaration MUST be included as the first item in an HTML document before the tag and is used to provide instructions to the web browser?",
+      choices: ["<caption>", "<code>", "<!DOCTYPE>", "<embed>"],
+      answer: "<!DOCTYPE>"
+    },
+    {
+      title: "What is the name of the group of properties that allows you to control the height and width of elements?",
+      choices: [
+        "Block",
+        "Size",
+        "Box",
+        "Dimension"
+      ],
+      answer: "Box"
+    },
+    {
+      title:
+        "All HTML elements are considered what?",
+      choices: [
+        "Tables",
+        "Boxes",
+        "Objects",
+        "Code"
+      ],
+      answer: "Code"
+    },
+    {
+      title: "What is the most important CSS property, used for controlling the layout?",
+      choices: [
+        "Margin",
+        "Table",
+        "<div>",
+        "Display"
+      ],
+      answer: "Display"
+    },
+    {
+      title: "In JavaScript, what element is used to store and manipulate text usually in multiples?",
+      choices: ["Variables", "Arrays", "Function", "Strings"],
+      answer: "Arrays"
+    },
+    {
+      title: "What is the default behavior called that is used to move declarations to the top of the current scope?",
+      choices: [
+        "Hoisting",
+        "Sorting",
+        "Arranging",
+        "Jumping"
+      ],
+      answer: "Hoisting"
+    },
+    {
+      title:
+        "What is the object called that lets you work with both dates and time-related data?",
+      choices: [
+        "Dates",
+        "Time-warp",
+        "Time field",
+        "Clock"],
+      answer: "Dates"
+    },
+    {
+      title:
+        "What can loops offer JavaScript code as a whole?",
+      choices: [
+      "Cross-platform support", 
+      "Improved Preformance", 
+      "Cleaner syntax",
+      "Added plug-ins"],
+      answer: "Improved Preformance"
+    },
+    {
+      title: "Which is used in full stack applications",
+      choices: ["HTML", "CSS", "JavaScript", "All the above"],
+      answer: "All the above"
+    }
+  ];
